570fcd390a720303aaac119c342edf30861b8836
honorific <honorific.info@gmail.com>
2023-04-04 11:13:20 +0430

    fix showing leaves

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import useSWR from 'swr'
-import ReactMapGL, {Marker, FlyToInterpolator} from 'react-map-gl'
+import ReactMapGL, {Marker} from 'react-map-gl'
+import supercluster from 'supercluster'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 import './App.css'
 import {useState} from 'react'
 import {useRef} from 'react'
-import useSupercluster from 'use-supercluster'
 import {useEffect} from 'react'
 
 function App() {
   const mapRef = useRef()
   const [bounds, setBounds] = useState([1, 1, 1, 1])
+  const [zoom, setZoom] = useState(0)
+  const [points, setPoints] = useState([])
+  const [clusters, setClusters] = useState([])
+  const [crimes, setCrimes] = useState([])
+  const superCluster = new supercluster({
+    radius: 70,
+    maxZoom: 20,
+  })
 
   const mouseDownHandler = () => {
     console.log('mouse down occurd good boy!')
   }
+
+  const handleZoom = (e) => {
+    setZoom(Math.round(e.viewState.zoom))
+    //superCluster.getLeaves(id)
+    console.log('zoom is: ', zoom)
+  }
+
   const fetcher = (...args) =>
     fetch(...args).then((response) => response.json())
 
@@ -22,47 +37,50 @@ function App() {
     'https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&data=2019-10'
 
   const {data, error} = useSWR(url, fetcher)
-  const crimes = data && !error ? data.slice(0, 2000) : []
-  const points = crimes.map((crime) => ({
-    type: 'Feature',
-    properties: {
-      cluster: false,
-      crimeId: crime.id,
-      category: crime.category,
-    },
-    geometry: {
-      type: 'Point',
-      coordinates: [crime.location.longitude, crime.location.latitude],
-    },
-  }))
+  useEffect(() => {
+    setCrimes(data && !error ? data.slice(0, 200) : [])
+    console.log('crime is: ', crimes)
+  }, [data])
+
+  useEffect(() => {
+    setPoints(
+      crimes.map((crime) => ({
+        type: 'Feature',
+        properties: {
+          cluster: false,
+          crimeId: crime.id,
+          category: crime.category,
+        },
+        geometry: {
+          type: 'Point',
+          coordinates: [crime.location.longitude, crime.location.latitude],
+        },
+      })),
+    )
+  }, [crimes])
 
   useEffect(() => {
     if (mapRef.current) {
       setBounds(mapRef?.current.getMap().getBounds().toArray().flat())
-    } else {
-      setBounds([-180, -85, 180, 85])
     }
   }, [mapRef?.current])
 
-  if (mapRef.current) {
-    console.log(
-      'mapRef is: ',
-      mapRef.current.getMap().getBounds().toArray().flat(),
-    )
-  }
-
   console.log('bounds are: ', bounds)
 
-  const {clusters} = useSupercluster({
-    points,
-    zoom: 12,
-    bounds,
-    options: {radius: 75, maxZoom: 20},
-  })
+  useEffect(() => {
+    superCluster.load(points)
+    console.log('points are: ', points)
+    setClusters(superCluster.getClusters(bounds, zoom))
+    console.log('clusters are: ', clusters)
+  }, [points, zoom, bounds])
 
-  console.log('first 200 crimes are: ', crimes)
-  console.log('clusters are: , ', clusters)
-  console.log('points are: ', points)
+  useEffect(() => {
+    if (mapRef.current) {
+      setBounds(mapRef.current.getMap().getBounds().toArray().flat())
+      console.log('bounds are: ', bounds)
+    }
+  }, [mapRef?.current])
+
   return (
     <div
       style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}
@@ -71,14 +89,14 @@ function App() {
         initialViewState={{
           latitude: 52.6376,
           longitude: -1.135171,
-          zoom: 12,
+          zoom,
         }}
-        on
         maxZoom={20}
         style={{width: '90vw', height: '90vh'}}
         mapStyle='mapbox://styles/mapbox/streets-v11'
         mapboxAccessToken={process.env.REACT_APP_MAP_TOKEN}
         onMouseDown={mouseDownHandler}
+        onZoomEnd={(e) => handleZoom(e)}
         ref={mapRef}
       >
         {clusters.map((cluster) => {
@@ -103,6 +121,17 @@ function App() {
                     color: 'white',
                     borderRadius: '100%',
                     textAlign: 'center',
+                  }}
+                  onClick={() => {
+                    const zoom = Math.min(
+                      superCluster.getClusterExpansionZoom(cluster.id),
+                      20,
+                    )
+                    mapRef.current.flyTo({
+                      center: [longitude, latitude],
+                      zoom,
+                      speed: 1,
+                    })
                   }}
                 >
                   {pointCount}
